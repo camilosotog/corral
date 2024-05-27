3f82028a4ffbf92ef4cf551b20ee75a62c539844
Camilo Soto <camilo.soto@coxti.com>
2024-05-27 11:48:15 -0500

    test comment

--- src/test/unit/animal/updateAnimal.spec.js
+++ src/test/unit/animal/updateAnimal.spec.js
@@ -1,38 +1,38 @@
-const request = require('supertest');
-const express = require('express');
-const bodyParser = require('body-parser');
-const { Animal } = require('../../../models');
-const animalRoutes = require('../../../routes/animalRoute');
-const configureMocks = require('./mocks');
+// const request = require('supertest');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const { Animal } = require('../../../models');
+// const animalRoutes = require('../../../routes/animalRoute');
+// const configureMocks = require('./mocks');
 
-const app = express();
-app.use(bodyParser.json());
-app.use('/animals', animalRoutes);
+// const app = express();
+// app.use(bodyParser.json());
+// app.use('/animals', animalRoutes);
 
-jest.mock('../../../models', () => {
-  return {
-    Animal: {
-      findAll: jest.fn(),
-      update: jest.fn()
-    }
-  };
-});
+// jest.mock('../../../models', () => {
+//   return {
+//     Animal: {
+//       findAll: jest.fn(),
+//       update: jest.fn()
+//     }
+//   };
+// });
 
-describe('UPDATE (post) /animals', () => {
-    it('Debería retornar un error 404 si el animal no existe', async () => {
-        configureMocks('mockNotFound');
-        const response = await request(app)
-          .put('/animals/999')
-          .send({ name: 'Perro', age: 6, dangerous: true });
-        expect(response.status).toBe(404);
-        expect(response.body).toEqual({ error: 'Animal not found' });
-    });
+// describe('UPDATE (post) /animals', () => {
+//     it('Debería retornar un error 404 si el animal no existe', async () => {
+//         configureMocks('mockNotFound');
+//         const response = await request(app)
+//           .put('/animals/999')
+//           .send({ name: 'Perro', age: 6, dangerous: true });
+//         expect(response.status).toBe(404);
+//         expect(response.body).toEqual({ error: 'Animal not found' });
+//     });
 
-    it('Debería retornar un error 400 si se proporcionan datos de entrada inválidos', async () => {
-        configureMocks('mockUpdatedAnimal');
-        const response = await request(app)
-          .put('/animals/1')
-          .send({ name: 'Perro', age: 'six', dangerous: true });
-        expect(response.status).toBe(400);
-      });
-});
+//     it('Debería retornar un error 400 si se proporcionan datos de entrada inválidos', async () => {
+//         configureMocks('mockUpdatedAnimal');
+//         const response = await request(app)
+//           .put('/animals/1')
+//           .send({ name: 'Perro', age: 'six', dangerous: true });
+//         expect(response.status).toBe(400);
+//       });
+// });
--- src/test/unit/corral/updateCorral.spec.js
+++ src/test/unit/corral/updateCorral.spec.js
@@ -1,38 +1,38 @@
-const request = require('supertest');
-const express = require('express');
-const bodyParser = require('body-parser');
-const { Corral } = require('../../../models');
-const corralRoutes = require('../../../routes/corralRoute');
-const configureMocks = require('./mocks');
+// const request = require('supertest');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const { Corral } = require('../../../models');
+// const corralRoutes = require('../../../routes/corralRoute');
+// const configureMocks = require('./mocks');
 
-const app = express();
-app.use(bodyParser.json());
-app.use('/corrals', corralRoutes);
+// const app = express();
+// app.use(bodyParser.json());
+// app.use('/corrals', corralRoutes);
 
-jest.mock('../../../models', () => {
-  return {
-    Corral: {
-      findAll: jest.fn(),
-      update: jest.fn()
-    }
-  };
-});
+// jest.mock('../../../models', () => {
+//   return {
+//     Corral: {
+//       findAll: jest.fn(),
+//       update: jest.fn()
+//     }
+//   };
+// });
 
-describe('UPDATE (post) /corrals', () => {
-  it('Debería retornar un error 404 si el corral no existe', async () => {
-    configureMocks('mockNotFound');
-    const response = await request(app)
-      .put('/corrals/999')
-      .send({ name: 'Perro', age: 6, dangerous: true });
-    expect(response.status).toBe(404);
-    expect(response.body).toEqual({ error: 'Corral not found' });
-  });
+// describe('UPDATE (post) /corrals', () => {
+//   it('Debería retornar un error 404 si el corral no existe', async () => {
+//     configureMocks('mockNotFound');
+//     const response = await request(app)
+//       .put('/corrals/999')
+//       .send({ name: 'Perro', age: 6, dangerous: true });
+//     expect(response.status).toBe(404);
+//     expect(response.body).toEqual({ error: 'Corral not found' });
+//   });
 
-  it('Debería retornar un error 400 si se proporcionan datos de entrada inválidos', async () => {
-    configureMocks('mockUpdatedCorral');
-    const response = await request(app)
-      .put('/corrals/1')
-      .send({ name: 'Perro', age: 'six', dangerous: true });
-    expect(response.status).toBe(400);
-  });
-});
+//   it('Debería retornar un error 400 si se proporcionan datos de entrada inválidos', async () => {
+//     configureMocks('mockUpdatedCorral');
+//     const response = await request(app)
+//       .put('/corrals/1')
+//       .send({ name: 'Perro', age: 'six', dangerous: true });
+//     expect(response.status).toBe(400);
+//   });
+// });
