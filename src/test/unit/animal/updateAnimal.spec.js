c0e54655dba0fcbbb09af073f2e20ad7cc91aca8
Camilo Soto <camilo.soto@coxti.com>
2024-05-27 21:25:03 -0500

    st

--- src/test/unit/animal/updateAnimal.spec.js
+++ src/test/unit/animal/updateAnimal.spec.js
@@ -19,20 +19,20 @@ jest.mock('../../../models', () => {
 });
 
 describe('UPDATE (post) /animals', () => {
-    it('Debería retornar un error 404 si el animal no existe', async () => {
-        configureMocks('mockNotFound');
-        const response = await request(app)
-          .put('/animals/999')
-          .send({ name: 'Perro', age: 6, dangerous: true });
-        expect(response.status).toBe(404);
-        expect(response.body).toEqual({ error: 'Animal not found' });
-    });
+  it('Debería retornar un error 404 si el animal no existe', async () => {
+      configureMocks('mockNotFound');
+      const response = await request(app)
+        .put('/animals/999')
+        .send({ name: 'Perro', age: 6, dangerous: true });
+      expect(response.status).toBe(404);
+      expect(response.body).toEqual({ error: 'Animal not found' });
+  });
 
-    it('Debería retornar un error 400 si se proporcionan datos de entrada inválidos', async () => {
-        configureMocks('mockUpdatedAnimal');
-        const response = await request(app)
-          .put('/animals/1')
-          .send({ name: 'Perro', age: 'six', dangerous: true });
-        expect(response.status).toBe(400);
-      });
+  it('Debería retornar un error 400 si se proporcionan datos de entrada inválidos', async () => {
+      configureMocks('mockUpdatedAnimal');
+      const response = await request(app)
+        .put('/animals/1')
+        .send({ name: 'Perro', age: 'six', dangerous: true });
+      expect(response.status).toBe(400);
+    });
 });
